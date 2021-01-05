f1a321946242bac672196e1e5b027b6f32fcdb48
Loosekonnection <david.bates@loosekonnection.co.uk>
2021-01-05 14:41:11 +0000

    10 Questions created in an object

---
+++ js/script.js
@@ -0,0 +1,102 @@
+var jsQuestions = [
+    {
+        question: "Who invented JavaScript?",
+        answers: {
+          a: "Douglas Crockford",
+          b: "Brendan Eich",
+          c: "Sheryl Sandberg",
+          d: "Linus Torvolds"
+        },
+        correctAnswer: "b"
+      },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            a: "JavaScript",
+            b: "Terminal / Bash",
+            c: "for loops",
+            d: "console.log"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answers: {
+            a: "commas",
+            b: "curly brackets",
+            c: "quotes",
+            d: "parentheses"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "ESLint",
+        d: "RequireJS",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "JavaScript objects are made up of which of the following",
+        answers: {
+            a: "Keys & Values",
+            b: "index & Values",
+            c: "for & forEach",
+            d: "Integers & Booleans"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: {
+            a: "The <head> section",
+            b: "The <body> section",
+            c: "The <footer> section",
+            d: "Both the <head> section and the <body> section are correct"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: {
+            a: "alertBox('Hello World');",
+            b: "alert('Hello World');",
+            c: "msgBox('Hello World');",
+            d: "prompt('Hello World');"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: {
+            a: "-",
+            b: "*",
+            c: "X",
+            d: "="
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        answers: {
+            a: "top(x, y)",
+            b: "ceil(x, y)",
+            c: "Math.max(x, y)",
+            d: "Math.ceil(x, y)"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "How does a FOR loop start??",
+        answers: {
+            a: "for (i = 0; i <= 5; i++)",
+            b: "for i = 1 to 5",
+            c: "for (i = 0; i <= 5)",
+            d: "for (i <= 5; i++)"
+        },
+        correctAnswer: "a"
+    },
+]
